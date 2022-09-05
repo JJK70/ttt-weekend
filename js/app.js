6ce4079ce6963443d66c77ed7bd1de80b3e707bb
JJK70 <jjk701@gmail.com>
2022-09-05 11:57:29 -0400

    Add handleClick function

--- js/app.js
+++ js/app.js
@@ -1,40 +1,72 @@
 /*-------------------------------- Constants --------------------------------*/
-
-
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board, turn, winner
 
 
-
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll('.game-square')
-const messageEl = document.getElementById('#message')
-
+const messageEl = document.querySelector('#message')
+// console.log(squareEls)
+const resetBtnEl = document.querySelector('#reset-button')
 
 /*----------------------------- Event Listeners -----------------------------*/
-
-
+squareEls.forEach(function(square) {
+  square.addEventListener('click', handleClick)
+})
+resetBtnEl.addEventListener('click', init)
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+
 function init() {
-  board [null,null,null,null,null,null,null,null,null]
+  board = [null, null, null, null, null, null, null, null, null]
   turn = 1
   winner = null
-  render()
-  console.log('')
+  render() 
+  resetBtnEl.setAttribute('hidden', true)
 }
 
-// loop board using index of the iteration to access the squares in squareEls array
-// style that square however you wish, dependent on the value contained in the current cell being iterated over (-1, 1, or null)
 function render() {
+  resetBtnEl.removeAttribute('hidden')
+  resetBtnEl.className = 'retry'
   board.forEach((square, index) => {
-   if(square === 1){
-    squareEls[index].textContent = 'X'
-   }if (square === -1){
-    squareEls[index].textContent = 'O'
-   } else if (square === null){
-    squareEls[index].textContent = ''
-   }
-  })
+    if(square === 1) {
+      squareEls[index].textContent = 'X' 
+    } if (square === -1) {
+      squareEls[index].textContent = 'O' 
+    } else if (square === null){
+      squareEls[index].textContent = ''
+    } 
+})
+  if (winner === null){
+    messageEl.textContent = ` It's player ${turn === 1 ? 'X' : 'O'} turn`
+  } else if (winner === "T"){
+    messageEl.textContent = `This game is a tie!`
+  } else {
+    messageEl.textContent = `It's player ${turn === 1 ? 'O' : 'X'} that wins the game!`
+  }
+}
+
+function handleClick(evt) {
+  const sqIdx = parseInt(evt.target.id.replace('sq', ''))
+  if(board[sqIdx] || winner !== null){
+    return
+  }
+  board[sqIdx] = turn
+  turn *= -1
+  winner = getWinner()
+  console.log(winner, "winner")
+  render()
+  console.log(board[sqIdx])
 }
